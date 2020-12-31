1f16fc40698e79f0474789fa6961d7f75eb62d4d
carlosgl93 <61713861+carlosgl93@users.noreply.github.com>
2020-12-31 13:00:15 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+
+import Header from './components/Header'
+import Loading from './screens/Loading'
+import Login from './screens/Login';
+import NewTintero from './screens/NewTintero';
+import Register from './screens/Register';
+import Tinteros from './screens/Tinteros';
+import User from './screens/User';
+import Tintero from './screens/Tintero'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Header/>
+    <AppNavigator/>
+    
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: Loading,
+  RegisterScreen: Register,
+  TinterosScreen: Tinteros,
+  LoginScreen: Login,
+  UserScreen: User,
+  CreateNewTinteroScreen: NewTintero,
+  ThisTintero: Tintero
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator)
+
+
